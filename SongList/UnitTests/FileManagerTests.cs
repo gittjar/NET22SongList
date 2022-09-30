f9e7b26a4331184a29d1935f60519709f864106e
Paavo Penttila <thxmuffe@hotmail.com>
2022-09-30 11:53:51 +0300

    Add new test for SongReader (not asserts yet)

---
+++ SongList/UnitTests/FileManagerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests;
+
+[TestClass]
+public class FileManagerTests
+{
+    [TestMethod]
+    public void SongListReader()
+    {
+        var reader = new FileManager.Classes.SongListReader();
+
+        var songs = reader.GetSongs();
+    }
+}
