ab8d5f358093c5589a20697140ab7cad8e62439a
Paavo Penttila <thxmuffe@hotmail.com>
2022-09-30 11:42:25 +0300

    Add csv parser for songs

--- SongList/FileManager/Classes/SongListReader.cs
+++ SongList/FileManager/Classes/SongListReader.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace FileManager
+namespace FileManager.Classes
 {
     public class SongListReader
     {
+        public IEnumerable<Song> GetSongs()
+        {
+            var lines = File.ReadAllLines("Files/songs.csv");
 
+            var list = new List<Song>();
+            foreach (var line in lines)
+            {
+                var values = line.Split(";");
+                list.Add(new Song()
+                {
+                    Artist = values[0]
+                });
+            }
+
+            return list;
+        }
     }
 }
 
